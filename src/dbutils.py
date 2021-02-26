a9dec63919b7a834dae10a7a0510d33554fb047d
Giovane Moura <giovane.moura@sidn.nl>
2021-02-26 15:39:37 +0100

    fixing error

--- src/dbutils.py
+++ src/dbutils.py
@@ -9,7 +9,7 @@ def conn_impala(server1,port1,ssl1,auth1):
     try:
         return impala.dbapi.connect(host=server1, port=port1, use_ssl=ssl1,auth_mechanism=auth1)
     except:
-        e = sys.exc_info()[0]
-        print("Error in connectImpala: " + str(e))
+        e = sys.exc_info()
+        print("Error in conn_impala: " + str(e))
 
 
