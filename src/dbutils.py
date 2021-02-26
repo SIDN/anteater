f2ca222d92855bf228553038702ae24ee7fbd762
Giovane Moura <giovane.moura@sidn.nl>
2021-02-26 15:26:14 +0100

    working on ini file

---
+++ src/dbutils.py
@@ -0,0 +1,16 @@
+import impala.dbapi
+from impala.dbapi import connect
+import calendar
+import psycopg2
+import logging
+import sys
+
+def conn_impala(server,port,ssl,auth):
+    conn=''
+    try:
+        return impala.dbapi.connect(host=server, port=port, use_ssl=ssl,auth_mechanism=auth)
+    except:
+        e = sys.exc_info()[0]
+        print("Error in connectImpala: " + e)
+
+
---
+++ src/test.py
@@ -0,0 +1,12 @@
+import configparser
+from dbutils import conn_impala
+
+cfg=configparser.ConfigParser()
+cfg.read('anteater-debug.ini')
+
+server=cfg['impala']['server']
+port=cfg['impala'].getint('port')
+ssl=cfg['impala'].getboolean('use_ssl')
+auth=cfg['impala']['auth_mechanism']
+
+conn=conn_impala(server,port,ssl,auth)
