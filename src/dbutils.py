186ececff008e4b3ff3c5ecef9fd91ff9fabc373
Giovane Moura <giovane.moura@sidn.nl>
2021-02-26 15:57:07 +0100

    adding postgresql conn

--- src/dbutils.py
+++ src/dbutils.py
@@ -13,3 +13,11 @@ def conn_impala(server1,port1,ssl1,auth1):
         print("Error in conn_impala: " + str(e))
 
 
+def conn_postgresql(host1,database1,port1,user1, password1):
+    try:
+        conn = psycopg2.connect(host=host1, database=database1, port=port1, user=user1,
+                                password=password1)
+        return conn
+    except:
+        e = sys.exc_info()
+        print("Error in conn_postgresql: " + str(e))
