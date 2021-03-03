5c0dcfc06ea13c5618497ce4b0293f6f280544e1
Giovane Moura <giovane.moura@sidn.nl>
2021-03-03 14:10:52 +0100

    first tryout dashboard

--- src/grafana-dashboards/stats_per_site/dashboard-sites.py
+++ src/grafana-dashboards/stats_per_site/dashboard-sites.py
@@ -46,10 +46,10 @@ def get_server_names(pars):
 
     entrada_db_table = pars['entrada']['database'] + "." + pars['entrada']['table']
 
-    entradaQuery = ''' select server,ipv  from '''
+    entradaQuery = ''' select server,server_location,ipv  from '''
     entradaQuery = entradaQuery + entrada_db_table + " where year="
     entradaQuery = entradaQuery + str(year) + " AND  month=" + str(month) + " and  day=" + str(day) +\
-                   "  group by server,ipv;"
+                   "  group by server,server_location,ipv;"
     cursor = "-1"
 
     conn = conn_impala()
@@ -85,15 +85,15 @@ def get_server_names(pars):
         # select server,ipv, count(1) as  nqueries,avg(tcp_hs_rtt) from
         for k in cursor:
             server = str(k[0])
-            ipv = str(k[1])
-            results.append(server+","+ipv)
+            server_location=str(k[1])
+            ipv = str(k[2])
+            results.append(server+"-"+server_location+"-"+ipv)
     cursor.close()
     conn.close()
     sorted(results)
-    #print("DEBUG\n results are\n"+ str(results))
     return results
 
-def makeQuery(pars,firstPanel,server_names):
+def makeQuery(pars,firstPanel,server_name,sites,ipv):
 
     #targets is a grafana list. each element in a list is a dict that represents a single line in a graph
     #like, graph for ns1.X.com IPv6 queries
@@ -104,34 +104,25 @@ def makeQuery(pars,firstPanel,server_names):
     demoTS=targets[0]
     alphabet=list(string.ascii_uppercase)
 
-
-
+    pg_db = pars['postgresql']['database']
     demoQuery=demoTS['rawSql']
     counter=0
     #now we need to create a new demoTS for each element in k
     # and update the variables accordingly
-    pg_db=pars['postgresql']['database']
-    for k in server_names:
-        sp=k.split(",")
+    for singleSite in sites:
+
         demoTS = targets[0]
         tempTS = demoTS.copy()
         demoQuery = demoTS['rawSql']
-        tempServer=sp[0].strip()
-        ipv=sp[1].strip()
-        if '4' in ipv:
-            ipv=4
-        elif '6' in ipv:
-            ipv=6
-        label=k
+
+        label=singleSite+"-IPv"+str(ipv)
 
         copyDemoTS=demoTS
-        #tempSQL=singleLine['rawSql']
-        #print(tempSQL)
-
         newQuery=demoQuery.replace("$LABEL", label)
         #print(tempSQL)
-        newQuery=newQuery.replace("$SERVER_NAME", tempServer)
+        newQuery=newQuery.replace("$SERVER_NAME", server_name)
         newQuery=newQuery.replace("$IPV",str(ipv))
+        newQuery = newQuery.replace("$SITE", str(singleSite))
         tempTS['rawSql']=newQuery
         #each line has its own id, alphabet sequence, so we need to get it
         tempTS['refId']=alphabet[counter]
@@ -142,43 +133,79 @@ def makeQuery(pars,firstPanel,server_names):
     firstPanel['datasource']=pg_db
     return firstPanel
 
+def makeServerPanels(server,sites,pars):
+
+    baseJSON = ''
+    with open('template/template.json') as f:
+        baseJSON = json.load(f)
+
+    # extract all panels before adding stuff
+    queriesPanel = baseJSON['panels'][0]
+    rttPanel = baseJSON['panels'][1]
+    resolversPanel = baseJSON['panels'][2]
+    asesPanel = baseJSON['panels'][2]
+
+    # now, we will need to generate a v4 and v6 version for each of them
+
+    makeQuery(pars, queriesPanel,server, sites,4)
+    baseJSON['panels'][0] =makeQuery(pars, queriesPanel,server, sites,4)
+    # then v6
+    baseJSON['panels'][1] = makeQuery(pars, queriesPanel,server, sites,6)
+
+    # second panel, rtt
+    baseJSON['panels'][2] = makeQuery(pars, rttPanel,server, sites,4)
+    baseJSON['panels'][3] = makeQuery(pars, rttPanel, server, sites, 6)
+
+    #resolvers
+    baseJSON['panels'][4] = makeQuery(pars, resolversPanel, server, sites, 4)
+    baseJSON['panels'][5] = makeQuery(pars, resolversPanel, server, sites, 6)
+
+    #    ases
+    baseJSON['panels'][6] = makeQuery(pars, asesPanel, server, sites, 4)
+    baseJSON['panels'][7] = makeQuery(pars, asesPanel, server, sites, 6)
+
+    with open('export/' + server+ '.json', 'w') as f:
+        json.dump(baseJSON,f)
+    print("Dashboard generated. Import export/"  + server+ ".json into Grafana and enjoy it !")
 
 
 def main():
 
     pars=read_ini()
 
-    server_names = get_server_names(pars)
+    #server_names = get_server_names(pars)
+
+    server_names=[]
+    server_names.append("ns1.dns.nl-london-ipv4")
+    server_names.append("ns1.dns.nl-ams-ipv6")
+    server_names.append("ns3.dns.nl-brz-ipv6")
+    servers=set()
+    serverSite=dict()
+
+    for k in server_names:
+        sp=k.split('-')
+        localServer=sp[0].strip()
+        servers.add(localServer)
+        site=sp[1].strip()
+        if localServer not in serverSite:
+            serverSite[localServer]=[]
+
+        tempArray=serverSite[localServer]
+        tempArray.append(site)
+        serverSite[localServer]=tempArray
 
 
-    baseJSON=''
-    with open('templates/authservers.json') as f:
+    #we will need to generate a  dashboard PER server
+    dashboards=[]
+    for eachServer in servers:
+        jsonFile=makeServerPanels(eachServer,serverSite[eachServer],pars)
 
-        baseJSON=json.load(f)
-
-    firstPanel=baseJSON['panels'][0]
-    tempPanel=makeQuery(pars,firstPanel,server_names)
-    baseJSON['panels'][0]=tempPanel
-
-    #second panel
-    secondPanel=baseJSON['panels'][1]
-    tempPanel = makeQuery(pars, secondPanel, server_names)
-    baseJSON['panels'][1]=tempPanel
-
-    thirdPanel=baseJSON['panels'][2]
-    tempPanel = makeQuery(pars, thirdPanel, server_names)
-    baseJSON['panels'][2]=tempPanel
-
-    lastPanel=baseJSON['panels'][3]
-    tempPanel = makeQuery(pars, lastPanel, server_names)
-    baseJSON['panels'][3]=tempPanel
+        with open('export/' + eachServer+ ".json", 'w') as f:
+            json.dump(jsonFile,f)
+        f.close()
 
 
-    with open('export/authservers.json', 'w') as f:
-        json.dump(baseJSON,f)
-    print("Dashboard generated. Import export/authservers.json into Grafana and enjoy it !")
-
-
+    print("Dashboards generated. Import export/*.json nto Grafana and enjoy it !")
 if __name__ == "__main__":
 
     if len(sys.argv) != 1:
